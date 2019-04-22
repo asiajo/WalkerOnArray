fb0a386d2816de78898ce5c7260ba0dc7f8cb378
Robert Kumanek <academy.7.student.ext@nokia.com>
2019-04-22 15:51:32 +0200

    Change standard I/O to NCurses's I/O

--- src/MoverOnArr.cpp
+++ src/MoverOnArr.cpp
@@ -1,4 +1,6 @@
-#include"MoverOnArr.hpp"
+#include "MoverOnArr.hpp"
+#include <ncurses.h>
+#include <unistd.h>
 
 Mover::Mover( int size,
               char background,
@@ -23,15 +25,61 @@ void Mover::move()
     space [ walker_position ] = walker;
 }
 
-void Mover::play()
+int kbhit(void)
 {
-    while( 1 )
-    {
-        system("clear");
-        for( const auto & elem : space )
-            std::cout << elem;
-        std::cout << std::endl;
-        move();
-        std::this_thread::sleep_for( std::chrono::seconds(1) );
+    int ch = getch();
+
+    if (ch != ERR) {
+        ungetch(ch);
+        return 1;
+    } else {
+        return 0;
     }
 }
+
+void Mover::play()
+{
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+
+    scrollok(stdscr, TRUE);
+    while (1) {
+
+        if (kbhit()) {
+            int newdir = getch();
+            if ( newdir == 97 )
+                dir = Direction::Left;
+            if ( newdir == 100 )
+                dir = Direction::Right;
+            if ( newdir == 113 )
+                break;
+            refresh();
+        }
+        move();
+        for( const auto & elem : space )
+            {printw( "%c", elem ); refresh();}
+        printw("\n");
+        refresh();
+        sleep(1);
+    }
+    endwin();
+}
+
+
+
+
+
+// void Mover::play()
+// {
+//     while( 1 )
+//     {
+//         system("clear");
+//         for( const auto & elem : space )
+//             std::cout << elem;
+//         std::cout << std::endl;
+//         move();
+//         std::this_thread::sleep_for( std::chrono::seconds(1) );
+//     }
+// }
